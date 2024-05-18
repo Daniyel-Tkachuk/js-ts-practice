395531fc9eeeef23dee909224d9d52fff4ea752d
Daniyel Tkachuk <danilaradyn7357899@gmail.com>
2024-05-18 13:21:11 +0300

    added new tests

--- src/09_immutability/09.test.ts
+++ src/09_immutability/09.test.ts
@@ -1,10 +1,11 @@
 import {
+   addCompany,
    addNewBooksToUser,
    makeHairstyle,
-   moveUser,
+   moveUser, NewObjectCompanies, removeBook, updateBooks, updateCompanyTitle, updateCompanyTitle2,
    upgradeUserLaptop,
    UserType,
-   UserWithBooksType,
+   UserWithBooksType, UserWithCompaniesType,
    UserWithLaptopType
 } from "./09";
 
@@ -94,4 +95,128 @@ test("add new books to user", () => {
    expect(result.books[4]).toBe("ts");
    expect(result.books[5]).toBe("restAPI");
    expect(user.books.length).not.toBe(result.books.length);
+});
+
+test("change books to user", () => {
+   const user: UserWithLaptopType & UserWithBooksType = {
+      name: "Daniyel",
+      hair: 32,
+      address: {
+         city: "Minsk",
+         house: 6
+      },
+      laptop: {
+         title: "Lenovo"
+      },
+      books: ["css", "js", "html", "react"],
+   };
+
+   const result = updateBooks(user, "js", "ts");
+
+   expect(user).not.toBe(result);
+   expect(user.books).not.toBe(result.books)
+   expect(user.address).toBe(result.address);
+   expect(result.laptop).toBe(user.laptop);
+   expect(result.books[1]).toBe("ts");
+   expect(user.books.length).toBe(4);
+   expect(result.books.length).toBe(4)
+});
+
+test("remove js book to user", () => {
+   const user: UserWithLaptopType & UserWithBooksType = {
+      name: "Daniyel",
+      hair: 32,
+      address: {
+         city: "Minsk",
+         house: 6
+      },
+      laptop: {
+         title: "Lenovo"
+      },
+      books: ["css", "js", "html", "react"],
+   };
+
+   const result = removeBook(user, "js");
+
+   expect(user).not.toBe(result);
+   expect(user.books).not.toBe(result.books)
+   expect(user.address).toBe(result.address);
+   expect(result.laptop).toBe(user.laptop);
+   expect(result.books.length).toBe(3);
+   expect(user.books.length).toBe(4);
+});
+
+test("add new company to user", () => {
+   const user: UserWithLaptopType & UserWithCompaniesType = {
+      name: "Daniyel",
+      hair: 32,
+      address: {
+         city: "Minsk",
+         house: 6
+      },
+      laptop: {
+         title: "Lenovo"
+      },
+      companies: [
+         {id: 1, title: "Epam"},
+         {id: 2, title: "it-incubator"}
+      ]
+   };
+
+   const result = addCompany(user, "Google");
+
+   expect(user).not.toBe(result);
+   expect(user.address).toBe(result.address);
+   expect(result.laptop).toBe(user.laptop);
+   expect(user.companies.length).toBe(2);
+   expect(result.companies.length).toBe(3);
+   expect(result.companies[2].title).toBe("Google");
+});
+
+test("update company for user", () => {
+   const user: UserWithLaptopType & UserWithCompaniesType = {
+      name: "Daniyel",
+      hair: 32,
+      address: {
+         city: "Minsk",
+         house: 6
+      },
+      laptop: {
+         title: "Lenovo"
+      },
+      companies: [
+         {id: 1, title: "Епам"},
+         {id: 2, title: "it-incubator"}
+      ]
+   };
+
+   const result = updateCompanyTitle(user, 1, "Epam");
+
+   expect(user).not.toBe(result);
+   expect(user.address).toBe(result.address);
+   expect(result.laptop).toBe(user.laptop);
+   expect(user.companies).not.toBe(result.companies);
+   expect(user.companies[0].title).toBe("Епам");
+   expect(result.companies[0].title).toBe("Epam");
+   expect(result.companies[0]).not.toBe(user.companies[0]);
+});
+
+test("update company title", () => {
+   const companies: NewObjectCompanies = {
+      "Dimych": [
+         {id: 1, title: "Epam"},
+         {id: 2, title: "Google"},
+      ],
+      "Daniyel": [
+         {id: 3, title: "It-incubator"},
+         {id: 4, title: "Яндекс"}
+      ]
+   };
+
+   const result = updateCompanyTitle2(companies, "Daniyel", 4, "Yandex");
+
+   expect(companies).not.toBe(result);
+   expect(companies["Daniyel"]).not.toBe(result["Daniyel"]);
+   expect(companies["Daniyel"][1].title).toBe("Яндекс");
+   expect(result["Daniyel"][1].title).toBe("Yandex");
 });
--- src/09_immutability/09.ts
+++ src/09_immutability/09.ts
@@ -19,6 +19,14 @@ export type UserWithBooksType = UserType & {
    books: string[]
 }
 
+export type UserWithCompaniesType = UserType & {
+   companies: Array<{id: number, title: string}>
+}
+
+export type NewObjectCompanies = {
+   [key: string]: {id: number, title: string}[]
+}
+
 
 export const makeHairstyle = (u: UserType, power: number) => {
    return {
@@ -51,5 +59,42 @@ export const addNewBooksToUser = (u: UserWithBooksType & UserWithLaptopType, arr
    return {
       ...u,
       books: [...u.books, ...arr]
+   }
+};
+
+export const updateBooks = (u: UserWithBooksType & UserWithLaptopType,
+                            oldBook: string,
+                            newBook: string) => {
+  return {
+     ...u,
+     books: u.books.map(b => b === oldBook ? newBook : b)
+  }
+};
+
+export const removeBook = (u: UserWithBooksType & UserWithLaptopType, bookForDelete: string,) => {
+   return {
+      ...u,
+      books: u.books.filter(b => b !== bookForDelete)
+   }
+};
+
+export const addCompany = (u: UserWithLaptopType & UserWithCompaniesType, company: string) => {
+   return {
+      ...u,
+      companies: [...u.companies, {id: 3, title: company}]
+   }
+}
+
+export const updateCompanyTitle = (u: UserWithLaptopType & UserWithCompaniesType, companyId: number, newTitle: string) => {
+   return {
+      ...u,
+      companies: u.companies.map(c => c.id === companyId ? {...c, title: newTitle} : c)
+   }
+};
+
+export const updateCompanyTitle2 = (company: NewObjectCompanies, userName: string, companyId: number, newTitle: string) => {
+   return {
+      ...company,
+      [userName]: company[userName].map(c => c.id === companyId ? {...c, title: newTitle} : c)
    }
 }
